fe3a12d2e401e53bb8befb91ad4b76d9b68405f6
AlexandrShnipov <sshnipov@gmail.com>
2022-07-12 18:08:08 +0300

    corrected MovieCard.svelte

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -4,30 +4,27 @@
 
 <div class="movie-card">
     <a sveltekit:prefetch sveltekit:noscroll href={`/movie/${movie.id}`}>
-    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}>
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}>
         <div class="description">
             <h2>{movie.title}</h2>
             <p>{movie.release_date}</p>
         </div>
     </a>
-
 </div>
 
 
 <style>
-
-    a{
-        display: block;
-        height: 85%;
-        margin-bottom: 1rem;
-    }
-
-    .movie-card{
-        position: relative;
+    .movie-card {
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
         padding: 1rem;
+    }
+
+    a {
+        display: block;
+        height: 85%;
+        margin-bottom: 1rem;
     }
 
     img {
@@ -39,24 +36,24 @@
         transition: all .8s linear;
     }
 
-    img:hover{
+    .movie-card:hover img{
         transform: scale(1.05);
     }
 
-h2{
-    font-size: .9rem;
-    margin-bottom: .5rem;
-    transition: all .8s linear;
-}
+    h2 {
+        font-size: .9rem;
+        margin-bottom: .5rem;
+        transition: all .8s linear;
+    }
 
-p{
-    font-size: .7rem;
-    transition: all .8s linear;
-}
+    p {
+        font-size: .7rem;
+        transition: all .8s linear;
+    }
 
-h2:hover,
-p:hover{
-    opacity: .6;
-}
+    h2:hover,
+    p:hover {
+        opacity: .6;
+    }
 
 </style>
